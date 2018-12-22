0a5309017297e1d1eb06dbadbe52ad0faf0d6552
Leong Wai Yin <gashzack@hotmail.com>
2018-12-22 14:16:41 +0800

    Update eslintrc with jest env to fix eslint warnings

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   parser: "babel-eslint",
   extends: "airbnb",
   env: {
-    browser: true // allows browser variables, e.g. "document.xxx"
+    browser: true, // allows browser variables, e.g. "document.xxx"
+    jest: true // fixes no-def and other jest env errors in test files
   }
 };
