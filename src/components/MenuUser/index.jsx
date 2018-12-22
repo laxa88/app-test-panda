e189477043fedf2b9f78e4f3910540476cd88b71
Leong Wai Yin <gashzack@hotmail.com>
2018-12-22 15:15:55 +0800

    Add initial MenuUser component

---
+++ src/components/MenuUser/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const MenuUser = () => (
+  <div>
+    <FontAwesomeIcon icon={['fas', 'user']} />
+    John | Log out
+  </div>
+);
+
+export default MenuUser;
---
+++ src/components/MenuUser/index.test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import MenuUser from './index';
+
+describe('<MenuUser />', () => {
+  it('renders', () => {
+    const result = renderer.create(<MenuUser />);
+    expect(result.toJSON()).toMatchSnapshot();
+  });
+});
