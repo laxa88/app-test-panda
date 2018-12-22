669ccde7f52b33a2bfd21dab93d336514bef1403
Leong Wai Yin <gashzack@hotmail.com>
2018-12-22 21:24:38 +0800

    Add story for ButtonSecondary

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,6 +3,7 @@ import { configure } from "@storybook/react";
 function loadStories() {
   require("../stories/Link.jsx");
   require("../stories/Button.jsx");
+  require("../stories/ButtonSecondary.jsx");
 }
 
 configure(loadStories, module);
---
+++ stories/ButtonSecondary.jsx
@@ -0,0 +1,32 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import ButtonSecondary from '../src/components/ButtonSecondary';
+import { sakura, ThemeContext } from '../src/theme';
+
+const callback = () => {
+  window.alert('Clicked!');
+};
+
+storiesOf('ButtonSecondary', module)
+  .add('default', () => (
+    <ButtonSecondary onClick={callback}>Click Me</ButtonSecondary>
+  ))
+  .add('default disabled', () => (
+    <ButtonSecondary onClick={callback} disabled>
+      Click Me
+    </ButtonSecondary>
+  ))
+  .add('sakura', () => (
+    <ThemeContext.Provider value={{ theme: sakura }}>
+      <ButtonSecondary onClick={callback}>Click Me</ButtonSecondary>
+    </ThemeContext.Provider>
+  ))
+  .add('sakura disabled', () => (
+    <ThemeContext.Provider value={{ theme: sakura }}>
+      <ButtonSecondary onClick={callback} disabled>
+        Click Me
+      </ButtonSecondary>
+    </ThemeContext.Provider>
+  ));
