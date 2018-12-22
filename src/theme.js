c90273efdd790b892435c4baff2021a5e0e3a5ad
Leong Wai Yin <gashzack@hotmail.com>
2018-12-22 21:22:27 +0800

    Add initial additional theme colours

--- src/theme.js
+++ src/theme.js
@@ -2,12 +2,18 @@ import React from 'react';
 
 export const panda = {
   primaryColor: '#33A00E',
+  secondaryColor: '#C5DFBC',
+  tertiaryColor: '#DDDDDD',
+  quaternaryColor: '#999999',
 };
 
 export const sakura = {
   primaryColor: 'pink',
+  secondaryColor: 'orange',
+  tertiaryColor: 'yellow',
+  quaternaryColor: 'red',
 };
 
-export const ThemeContext = React.createContext({
-  theme: panda,
-});
+export const currentTheme = panda;
+
+export const ThemeContext = React.createContext({ theme: currentTheme });
