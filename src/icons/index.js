d43c71397affeafdbccfebe23f877f5d001e5a4e
Leong Wai Yin <gashzack@hotmail.com>
2018-12-22 13:12:50 +0800

    Add fontawesome library setup

---
+++ src/icons/index.js
@@ -0,0 +1,26 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faFutbol,
+  faLifeRing,
+  faTrashAlt,
+} from '@fortawesome/free-regular-svg-icons';
+import {
+  faBars,
+  faBicycle,
+  faHome,
+  faPuzzlePiece,
+  faRecycle,
+  faUser,
+} from '@fortawesome/free-solid-svg-icons';
+
+library.add(
+  faBars,
+  faBicycle,
+  faFutbol,
+  faHome,
+  faLifeRing,
+  faPuzzlePiece,
+  faRecycle,
+  faTrashAlt,
+  faUser,
+);
--- src/index.jsx
+++ src/index.jsx
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './app';
+import './icons';
 
 ReactDOM.render(<App />, document.getElementById('app'));
