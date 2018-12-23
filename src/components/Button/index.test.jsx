5fcac387b542e5f1876fb11db547d0569d217b7c
Leong Wai Yin <gashzack@hotmail.com>
2018-12-23 12:44:35 +0800

    Remove unnecessary theme test for Button and Input

--- src/components/Button/index.test.jsx
+++ src/components/Button/index.test.jsx
@@ -3,10 +3,10 @@ import renderer from 'react-test-renderer';
 import 'jest-styled-components';
 
 import Button from './index';
-import { currentTheme, ThemeContext } from '../../theme';
+import { currentTheme } from '../../theme';
 
 describe('<Button />', () => {
-  it('renders (default)', () => {
+  it('renders', () => {
     const result = renderer.create(<Button />);
 
     expect(result.toJSON().type).toBe('button');
@@ -25,36 +25,6 @@ describe('<Button />', () => {
     expect(result.toJSON()).toHaveStyleRule(
       'background-color',
       currentTheme.primaryColor1,
-      { modifier: ':disabled:hover' },
-    );
-  });
-
-  it('renders (themed)', () => {
-    const mockTheme = {
-      primaryColor1: 'dummy-color-1',
-      primaryColor2: 'dummy-color-2',
-    };
-
-    const result = renderer.create(
-      <ThemeContext.Provider value={{ theme: mockTheme }}>
-        <Button />
-      </ThemeContext.Provider>,
-    );
-
-    expect(result.toJSON()).toHaveStyleRule(
-      'background-color',
-      mockTheme.primaryColor1,
-    );
-
-    expect(result.toJSON()).toHaveStyleRule(
-      'background-color',
-      mockTheme.primaryColor2,
-      { modifier: ':hover' },
-    );
-
-    expect(result.toJSON()).toHaveStyleRule(
-      'background-color',
-      mockTheme.primaryColor1,
       { modifier: ':disabled:hover' },
     );
   });
--- src/components/Input/index.test.jsx
+++ src/components/Input/index.test.jsx
@@ -3,10 +3,10 @@ import renderer from 'react-test-renderer';
 import 'jest-styled-components';
 
 import Input from './index';
-import { currentTheme, ThemeContext } from '../../theme';
+import { currentTheme } from '../../theme';
 
 describe('<Input />', () => {
-  it('renders (default)', () => {
+  it('renders', () => {
     const result = renderer.create(<Input />);
 
     expect(result.toJSON().type).toBe('input');
@@ -27,40 +27,6 @@ describe('<Input />', () => {
     expect(result.toJSON()).toHaveStyleRule(
       'border-color',
       currentTheme.secondaryColor1,
-      { modifier: ':disabled' },
-    );
-  });
-
-  it('renders (themed)', () => {
-    const mockTheme = {
-      primaryColor1: 'dummy-color-1',
-      secondaryColor1: 'dummy-color-2',
-      textColor1: 'dummy-color-3',
-      textColor2: 'dummy-color-4',
-    };
-
-    const result = renderer.create(
-      <ThemeContext.Provider value={{ theme: mockTheme }}>
-        <Input />
-      </ThemeContext.Provider>,
-    );
-
-    expect(result.toJSON()).toHaveStyleRule(
-      'border-color',
-      mockTheme.textColor2,
-    );
-
-    expect(result.toJSON()).toHaveStyleRule('color', mockTheme.textColor1);
-
-    expect(result.toJSON()).toHaveStyleRule(
-      'border-color',
-      mockTheme.primaryColor1,
-      { modifier: ':focus' },
-    );
-
-    expect(result.toJSON()).toHaveStyleRule(
-      'border-color',
-      mockTheme.secondaryColor1,
       { modifier: ':disabled' },
     );
   });
