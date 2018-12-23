fa1fe334fe51e7488c5465bdc70178590673894c
Leong Wai Yin <gashzack@hotmail.com>
2018-12-24 00:25:00 +0800

    Remove unnecessary theme test in ButtonSecondary

--- src/components/ButtonSecondary/index.test.jsx
+++ src/components/ButtonSecondary/index.test.jsx
@@ -3,7 +3,7 @@ import renderer from 'react-test-renderer';
 import 'jest-styled-components';
 
 import ButtonSecondary from './index';
-import { currentTheme, ThemeContext } from '../../theme';
+import { currentTheme } from '../../theme';
 
 describe('<ButtonSecondary />', () => {
   it('renders (default)', () => {
@@ -27,38 +27,6 @@ describe('<ButtonSecondary />', () => {
     expect(result.toJSON()).toHaveStyleRule(
       'background-color',
       currentTheme.secondaryColor1,
-      { modifier: ':disabled:hover' },
-    );
-  });
-
-  it('renders (themed)', () => {
-    const spy = jest.fn();
-
-    const mockTheme = {
-      secondaryColor1: 'dummy-color-1',
-      secondaryColor2: 'dummy-color-2',
-    };
-
-    const result = renderer.create(
-      <ThemeContext.Provider value={{ theme: mockTheme }}>
-        <ButtonSecondary onClick={spy} />
-      </ThemeContext.Provider>,
-    );
-
-    expect(result.toJSON()).toHaveStyleRule(
-      'background-color',
-      mockTheme.secondaryColor1,
-    );
-
-    expect(result.toJSON()).toHaveStyleRule(
-      'background-color',
-      mockTheme.secondaryColor2,
-      { modifier: ':hover' },
-    );
-
-    expect(result.toJSON()).toHaveStyleRule(
-      'background-color',
-      mockTheme.secondaryColor1,
       { modifier: ':disabled:hover' },
     );
   });
