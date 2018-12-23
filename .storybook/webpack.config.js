5882723f8cceffe01fc1f928f483fbce3916163d
Leong Wai Yin <gashzack@hotmail.com>
2018-12-23 19:06:52 +0800

    Add storybook webpack config, inject dev webpack rules

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,16 @@
+const path = require("path");
+
+const devConfig = require("../webpack.dev.js");
+
+/*
+  Reference: https://storybook.js.org/configurations/custom-webpack-config/#full-control-mode--default
+
+  Storybook runs its own webpack-dev-server with its own default webpack.config.js.
+  In order for our app's components to load correctly (e.g. with CSS-loaders, etc),
+  we may need to override some variables, which we can do here.
+*/
+
+module.exports = (baseConfig, env, defaultConfig) => {
+  defaultConfig.module.rules = devConfig().module.rules;
+  return defaultConfig;
+};
