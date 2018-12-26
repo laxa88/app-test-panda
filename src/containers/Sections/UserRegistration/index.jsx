80f10cd84fe27e886a68fd31bbcbf21594a45726
laxa88 <gashzack@hotmail.com>
2018-12-27 00:37:19 +0800

    Fix incorrect daysOfWeeks labels

--- src/containers/Sections/UserRegistration/index.jsx
+++ src/containers/Sections/UserRegistration/index.jsx
@@ -17,13 +17,13 @@ const rideInGroupOptions = [
 ];
 
 const daysOfTheWeekOptions = [
-  { value: 0, label: 'Sun' },
-  { value: 1, label: 'Mon' },
-  { value: 2, label: 'Tue' },
-  { value: 3, label: 'Wed' },
-  { value: 4, label: 'Thu' },
-  { value: 5, label: 'Fri' },
-  { value: 6, label: 'Sat' },
+  { value: 0, label: 'Mon' },
+  { value: 1, label: 'Tue' },
+  { value: 2, label: 'Wed' },
+  { value: 3, label: 'Thu' },
+  { value: 4, label: 'Fri' },
+  { value: 5, label: 'Sat' },
+  { value: 6, label: 'Sun' },
 ];
 
 const initialState = {
